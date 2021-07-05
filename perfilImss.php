ce80cc0a136b8ffeb2ae5f969e3427037395aafb
Rous <rosita14640054@gmail.com>
2021-07-04 19:23:49 -0500

    se agregaron los estilos

--- perfilImss.php
+++ perfilImss.php
@@ -3,52 +3,43 @@
 <head>
 	<meta charset="utf-8"/>
 	<title>Carrito de Compras</title>
-	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
+	<!---<link rel="stylesheet" type="text/css" href="./css/estilos.css">-->
 	<link rel="stylesheet" type="text/css" href="./css/body.css">
-	<link rel="stylesheet" type="text/css" href="./css/producto.css">
 	<link rel="stylesheet" type="text/css" href="./css/footer.css">
 	<link rel="stylesheet" type="text/css" href="./css/redes_sociales.css">
     <link rel="stylesheet" type="text/css" href="./css/icons.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/agotado.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/perfil.css"/>
-      <link rel="stylesheet" type="text/css" href="./css/encabezado.css"/>
-       <link rel="stylesheet" type="text/css" href="agotado./colorPiedePagina.css"/>
-
+    <!--<link rel="stylesheet" type="text/css" href="./css/perfil.css"/>-->
+    
+       <link rel="stylesheet" type="text/css" href="./css/colorPiedePagina.css"/>
+  <link rel="stylesheet" type="text/css" href="./css/botonerPerfil.css"/>
     <link rel="stylesheet" type="text/css" href="./css/estilofondo.css">
 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript"  href="./js/scripts.js"></script>
 </head>
-<<<<<<< HEAD
 <body class="fondo">
 	<header class="encabezado"> 
-    <a href="./notificaciones.php" title="notificaciones">
-			<img src="./imagenes/carrito.png">
-=======
-<body >
-	<header>
-    <a href="./notificaciones.php" title="notificaciones" id="notificaciones">
-			<img src="notificaciones.png">
->>>>>>> master
+        <img src="./imagenes/logoH.png" id="logo">
+   
+		
 		</a>	
-		<a href="./ayuda.php" title="Ayuda" id="ayuda" >Ayuda</a>
-        <a href="./miInfo.php" title="Mi info" id="info" >Mi Informacion</a>
-        <a href="./elegirEnfermedad.php" title="Crear Cita" id="crearCita" >Crear cita</a>
-        <a href="./perfilImss.php" title="Mis citas" id="MisCitas" >Mis Citas</a>
-
-		
-
-        
-
-        
-
-		
-
-	</header>
-	
-
+</header>
 	<section>
+        <div>
+               <nav class="botonera">
+  <ul class="botones">
+    <li class="btn1"><a href="./ayuda.php" title="Ayuda" class="registrarse" >Ayuda</a></li>
+    <li class="btn1"> <a href="./miInfo.php" title="Mi info" class="registrarse" >Mi Informacion</a></li>
+    <li class="btn1"> <a href="./elegirEnfermedad.php" title="Crear Cita" class="registrarse" >Crear cita</a></li>
+    <li class="btn1"> <a href="./perfilImss.php" title="Mis citas" class="registrarse" >Mis Citas</a></li>
+
+
+  </ul>
+</nav>
+        </div>
+        
     <div >
+      
 			
             <div class="citas">
 	<?php
